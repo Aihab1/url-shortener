b8f38f804e49828f0bb383ea38994f1b6514eed5
Aihab Umair <addisonhardley111@gmail.com>
2025-03-15 17:40:26 +0530

    Add eureka server and api gateway

---
+++ eureka-server/src/test/java/com/url/shortener/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.shortener.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
